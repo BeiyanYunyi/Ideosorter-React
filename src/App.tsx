6c22bc7d8bf16436181c9f1f4e74352d7b5fdf8f
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2021-11-28 18:58:58 +0800

    fix pwa

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Typography } from '@mui/material';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
+import ReloadPrompt from './components/ReloadPrompt';
 import HomePage from './pages/HomePage';
 import InstructionPage from './pages/InstructionPage';
 import L10nPage from './pages/L10nPage';
@@ -8,12 +9,22 @@ import ResultPage from './pages/ResultPage';
 
 const App = () => (
   <Routes>
-    <Route path="/l10n/:l10n/instructions" element={<InstructionPage />} />
-    <Route path="/l10n/:l10n/results/:resultName" element={<ResultPage />} />
-    <Route path="/l10n/:l10n/questions/:questionName" element={<QuestionPage />} />
-    <Route path="/l10n/:l10n" element={<L10nPage />} />
-    <Route path="/" element={<HomePage />} />
-    <Route path="*" element={<Typography>404</Typography>} />
+    <Route
+      path="/"
+      element={
+        <>
+          <Outlet />
+          <ReloadPrompt />
+        </>
+      }
+    >
+      <Route path="/l10n/:l10n/instructions" element={<InstructionPage />} />
+      <Route path="/l10n/:l10n/results/:resultName" element={<ResultPage />} />
+      <Route path="/l10n/:l10n/questions/:questionName" element={<QuestionPage />} />
+      <Route path="/l10n/:l10n" element={<L10nPage />} />
+      <Route path="" element={<HomePage />} />
+      <Route path="*" element={<Typography>404</Typography>} />
+    </Route>
   </Routes>
 );
 
